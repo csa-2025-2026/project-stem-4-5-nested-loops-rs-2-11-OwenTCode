2508d89a838a4e461113380fe210191989b1827e
OwenTCode <Oweneight88@gmail.com>
2025-10-28 18:21:04 +0000

    2qwe

--- Main.java
+++ Main.java
@@ -4,8 +4,14 @@ public class Main
 {
   public static void main(String[] args)
   {
-    printNtimes("buh", 3);
-    printNums(10);
+    // printNtimes("buh", 3);
+    printNums();
+    System.out.print("Input an int: ");
+    Scanner input = new Scanner(System.in);
+    int blah = input.nextInt();
+    uprightNumberTriangle(blah);
+    starTree();
+
   }
 
   public static void printNTimes(String word, int N)
@@ -22,23 +28,46 @@ public class Main
 
   public static void printNums()
   {
-    for (int i = 10; i > 0; i--)
+    for (int rowNum = 10; rowNum > 0; rowNum--)
     {
-      for (i > 9)
+      for (int j = 0; j < rowNum; j++)
       {
-        System.out.print(i);
+        System.out.print(rowNum);
       }
+      System.out.println("");
     }
   }
 
   public static void uprightNumberTriangle(int N)
   {
-    // code solution here
+    for (int row = 1; row <= N; row++)
+    {
+      int wah = 1;
+      for (int count = 0; count < row; count++)
+      {
+        
+        System.out.print(wah + " ");
+        wah++;
+        
+      }
+      System.out.println("");
+    }
+
   }
 
   public static void starTree()
   {
-    // code solution here
+    for (int starCount = 10; starCount > 0; starCount--)
+    {
+      for (int row = starCount; row > 1; row--)
+      {
+        System.out.print("*");
+        System.out.print(" ");
+      }
+      System.out.println(" ");
+      
+      
+    }
   }
 
   public static void multTable()
